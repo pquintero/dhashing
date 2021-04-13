62c239bd599e6cf60e9a333d0a0e1696a35cff4a
Patricio Quintero <pquintero@Patricios-MacBook-Pro.local>
2021-04-13 10:48:15 -0400

    Add hashing to email and name

--- src/main/dashing.py
+++ src/main/dashing.py
@@ -1,48 +1,76 @@
-from pyspark import SparkConf, SparkContext, SQLContext
-from pyspark.sql.functions import udf
 import hashlib
-from pyspark.sql import SparkSession
-from pyspark.sql.types import StringType, IntegerType, StructType, StructField
-
+import os
 import pathlib
 
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import udf, lit
+from pyspark.sql.types import StringType
+
 CURRENT_PATH = pathlib.Path(__file__).parent.absolute()
+INPUT_FILE = os.path.abspath(os.path.join(CURRENT_PATH, '../resources/people.csv'))
+HASHING_OUTPUT_PATH = os.path.abspath(os.path.join(CURRENT_PATH, '../resources/hashing_output'))
+ACTIVATION_OUTPUT_PATH = os.path.abspath(os.path.join(CURRENT_PATH, '../resources/table_activation'))
 SALT = str("#8b%$K!z")
 
 
-def encrypt_value(hashing_key):
+def hashing_value(hashing_key):
     sha_value = hashlib.sha512((hashing_key + SALT).encode()).hexdigest()
 
     return sha_value
 
 
-spark_udf = udf(encrypt_value, StringType())
+hashing_udf = udf(hashing_value, StringType())
 
 if __name__ == '__main__':
-
     spark = SparkSession.builder \
         .master('local') \
         .appName('column_encryption') \
         .getOrCreate()
 
     df = spark.read.option("encoding", "UTF-8") \
-        .csv("/Users/pquintero/github-workspace/dhasing/src/resources/people.csv"
+        .csv(INPUT_FILE
              , sep=";"
              , inferSchema=True
              , header=True).cache()
 
-    df.printSchema()
-
-    # Encrypted Name
-    hashing = df.withColumn('encrypted_name', spark_udf('name'))
+    # Hashing Name
+    hashing = df.withColumn('hashed_name', hashing_udf('name'))
     hashing.show(truncate=False)
 
-    # Encrypted Email
-    hashing = hashing.withColumn('encrypted_email', spark_udf('email'))
+    # Saving Name Values to Activate
+    activation_name = hashing.withColumn("table", lit("user")).withColumn("key_type", lit("name"))
+    # activation_name.show(truncate=False)
+
+    activation_name\
+        .drop("email")\
+        .drop("age")\
+        .drop("job")\
+        .write.mode('append')\
+        .partitionBy("table", "key_type")\
+        .parquet(ACTIVATION_OUTPUT_PATH)
+
+    # Hashing Email
+    hashing = hashing.withColumn('hashed_email', hashing_udf('email'))
     hashing.show(truncate=False)
 
-    print("Bye !")
+    # Saving Email Values to Activate
+    activation_email = hashing.withColumn("table", lit("user")).withColumn("key_type", lit("email"))
+    # activation_email.show(truncate=False)
 
-    # Import the os module
-    import os
+    activation_email \
+        .drop("name") \
+        .drop("age") \
+        .drop("job") \
+        .drop("hashed_name") \
+        .write.mode('append') \
+        .partitionBy("table", "key_type") \
+        .parquet(ACTIVATION_OUTPUT_PATH)
 
+    # Saving new Table with Hashing values
+    hashing \
+        .drop("email") \
+        .drop("name") \
+        .write.mode('overwrite') \
+        .parquet(HASHING_OUTPUT_PATH)
+
+    print("End !")
